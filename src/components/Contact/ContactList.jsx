a4985d53e47e8ce0cdfe2b96c580ddfcc95c8215
Ronas Tiwari <120455889+Ronas1zz4@users.noreply.github.com>
2024-10-05 20:33:27 +0545

    ---

--- src/components/Contact/ContactList.jsx
+++ src/components/Contact/ContactList.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
+import React from "react";
 import { GoArrowRight } from "react-icons/go";
 
-const ContactList = () => {
+
+const ContactList = (props) => {
   return (
-    <div className='w-full contact_list text-black border-y border-black p-4 '>
-<div className='w-full flex items-center justify-between container ' >
- <a href=" "><p className='uppercase'>Github</p></a>
-
-<GoArrowRight/>
-</div>
+<>
+<div className="w-full contact_list text-black border-y border-black p-4">
+      <div className="w-full flex items-center justify-between container">
+        <a href="">
+          <p className="uppercase">GitHub</p>
+        </a>
+        <button className="">
+          <GoArrowRight className="-rotate-45" />
+        </button>
+      </div>
     </div>
-  )
-}
+</>
+  );
+};
 
-export default ContactList
+export default ContactList;
